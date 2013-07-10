489ac584e304b9a438e31a2f261aaad8d5447540
Ubuntu <ubuntu@ip-172-31-46-75.us-west-2.compute.internal>
2013-07-10 21:35:42 +0000

    Added buf.toString and readFileSync feature

--- web.js
+++ web.js
@@ -2,6 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
 var buf = fs.readFileSync(index.html);
 
 app.get('/', function(request, response) {
