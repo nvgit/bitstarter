b132aef4bfefec03e2d90728f60ea7271e77c6e4
Ubuntu <ubuntu@ip-172-31-46-75.us-west-2.compute.internal>
2013-07-10 22:23:23 +0000

    Added hw03-pt2 bitstarter index.html page

--- web.js
+++ web.js
@@ -3,7 +3,6 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 fs = require('fs');
-//fs.readFile('index.html', 'utf8')
 var buf = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
@@ -14,3 +13,4 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
